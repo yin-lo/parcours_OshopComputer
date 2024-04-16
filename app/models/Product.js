705e05280c50f0e22ddfe6be3912459941158649
yin-lo <yinlo@hotmail.fr>
2024-04-16 09:26:00 +0200

    refactor: :art: Model Product created

--- app/models/Product.js
+++ app/models/Product.js
@@ -13,4 +13,41 @@ class Product extends Sequelize.Model {}
  * tableName: 'products',
  */
 
+Product.init(
+	{
+		category_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+
+		ref: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+
+		image: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+
+		title: {
+			type:DataTypes.TEXT,
+			allowNull: false,
+		},
+		description: {
+			type:DataTypes.TEXT,
+			allowNull: false,
+		},
+
+		price: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+	},
+	{
+		sequelize,
+		tableName: 'products',
+	},
+);
+
 module.exports = Product;
