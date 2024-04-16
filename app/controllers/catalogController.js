07768ab0ad8abf05f390a6241c454e48e787843b
yin-lo <yinlo@hotmail.fr>
2024-04-16 11:01:38 +0200

    feat: :sparkles: Dynamic category page

--- app/controllers/catalogController.js
+++ app/controllers/catalogController.js
@@ -24,22 +24,40 @@ const catalogController = {
         try {
             const categoryId = Number(req.params.id);
             const category = await Category.findByPk(categoryId, {
-                association: 'products',
+                include:[
+                    { association: 'products',}
+                ],
             });
 
+            console.log(category);
             res.render('category', {
                 category,
             });
 
         } catch (error) {
             console.log(error);
-            res.status(500).send('Error with categories');
+            res.status(500).send('Error with categories and products');
         }
     },
 
     product: async (req, res) => {
         // todo, récupérer le produit demandé en base de données.
-        res.render('product');
+        try {
+            const categoryId = Number(req.params.id);
+            const products = await Product.findAll({
+                include: {
+                    association: 'category',
+                    attributes: ['id'],
+                },
+            });
+          
+            res.render('product', {
+                products,
+            });
+        } catch (error) {
+            console.log(error);
+            res.status(500).send('Error with products');
+        }
     },
 
     cart: (req, res) => {
